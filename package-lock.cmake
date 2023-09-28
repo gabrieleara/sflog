ec21e9d619db7b71cb4dc99479dec21caa70b338
Gabriele Ara <gabriele.ara@santannapisa.it>
2023-09-28 11:44:21 +0200

    Experimenting for installation

--- package-lock.cmake
+++ package-lock.cmake
@@ -6,10 +6,12 @@ CPMDeclarePackage(magic_enum
   NAME magic_enum
   GIT_TAG v0.9.3
   GITHUB_REPOSITORY Neargye/magic_enum
+  OPTIONS "MAGIC_ENUM_OPT_INSTALL YES"
 )
 # fmt
 CPMDeclarePackage(fmt
   NAME fmt
   GIT_TAG 10.1.1
   GITHUB_REPOSITORY fmtlib/fmt
+  OPTIONS "FMT_INSTALL ON"
 )
